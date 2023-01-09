30653161b9eacc785a208af770c008a5f0a99ad0
TotiFlame <santifer4321@gmail.com>
2023-01-09 18:10:52 +0000

    Task 4

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 apt-get update
-apt-get install hugo
+apt-get install wget
 apt-get install make
+wget https://github.com/gohugoio/hugo/releases/download/v0.81.0/hugo_0.81.0_Linux-64bit.deb
+dpkg -i hugo_0.81.0_Linux-64bit.deb
 make build
