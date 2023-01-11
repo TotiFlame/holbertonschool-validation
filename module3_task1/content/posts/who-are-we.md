e1ff5be4a0159aef57a21e3001e208af28ad8817
TotiFlame <santifer4321@gmail.com>
2023-01-11 17:22:36 +0000

    Task 1

---
+++ module3_task1/content/posts/who-are-we.md
@@ -0,0 +1,6 @@
+---
+title: "Who are we"
+date: 2023-01-07T04:16:58Z
+draft: true
+---
+
---
+++ module3_task1/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update
+apt-get install wget
+apt-get install make
+wget https://github.com/gohugoio/hugo/releases/download/v0.81.0/hugo_0.81.0_Linux-64bit.deb
+dpkg -i hugo_0.81.0_Linux-64bit.deb
