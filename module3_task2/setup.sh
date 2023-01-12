6ba30f6f77a6bfcdfe5994620eb713ef961e12ae
TotiFlame <santifer4321@gmail.com>
2023-01-12 00:20:18 +0000

    Task 2

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -3,4 +3,5 @@ apt-get install wget
 apt-get install make
 wget https://github.com/gohugoio/hugo/releases/download/v0.79.0/hugo_0.79.0_Linux-64bit.deb
 dpkg -i hugo_0.79.0_Linux-64bit.deb
+make build
 exit 0
